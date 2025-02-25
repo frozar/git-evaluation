172f6df7f63d6588b5ded776342898d52d5f5d88
AlexandreRama <ramalingom.alexandre5@gmail.com>
2025-02-25 21:21:11 +0400

    Ajout gestion d'erreur

--- minitrice.py
+++ minitrice.py
@@ -1,8 +1,9 @@
 import re
+import sys
 
 while True:
     try:
-        operation = input("> ")
+        operation = input("> " if sys.stdin.isatty() else "")
 
         if not re.match(r"^\s*\d+(\.\d+)?\s*[-+*/]\s*\d+(\.\d+)?\s*$", operation):
             raise SyntaxError(f"Erreur de syntaxe pour le calcul: \"{operation}\"")
@@ -17,11 +18,18 @@ while True:
             result = int(int1.strip()) * int(int2.strip())
         elif '/' in operation:
             int1, int2 = operation.split('/')
+            if int(int2.strip()) == 0:
+                raise ZeroDivisionError("Division par zéro")
             result = int(int1.strip()) / int(int2.strip())
         else :
             raise SyntaxError(f"Erreur de syntaxe pour le calcul: \"{operation}\"")
         
         print(result)
 
+    except (KeyboardInterrupt, EOFError):
+        if sys.stdin.isatty():
+            print("\n Fin des calculs :)")
+        sys.exit(0)
     except Exception as e:
         print(f"Erreur: {e}")
+        sys.exit(1)
