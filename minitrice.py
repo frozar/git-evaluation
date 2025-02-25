dacffc4e67568f2dee624940a6c28c9f0f93ad3c
Anais <anaisriviere03@gmail.com>
2025-02-25 19:42:58 +0400

    ajout du fichier minitrice qui gère + et -

---
+++ minitrice.py
@@ -0,0 +1,16 @@
+import re
+
+while True:
+    try:
+        operation = input(" ")
+
+        if not re.match(r"^\s*\d+(\.\d+)?\s*[-+*/]\s*\d+(\.\d+)?\s*$", operation):
+            raise SyntaxError(f"Erreur de syntaxe pour le calcul: \"{operation}\"")
+        if '+' in operation:
+            int1, int2 = operation.split('+')
+            result = int(int1.strip()) + int(int2.strip())
+        elif '-' in operation:
+            int1, int2 = operation.split('-')
+            result = int(int1.strip()) - int(int2.strip())
+
+    
