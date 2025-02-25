f9010abdddcdb3e7379613e2ef23a2a24111da22
AlexandreRama <ramalingom.alexandre5@gmail.com>
2025-02-25 20:45:32 +0400

    Ajout du * et / | fin du try/except

--- minitrice.py
+++ minitrice.py
@@ -2,7 +2,7 @@ import re
 
 while True:
     try:
-        operation = input(" ")
+        operation = input("> ")
 
         if not re.match(r"^\s*\d+(\.\d+)?\s*[-+*/]\s*\d+(\.\d+)?\s*$", operation):
             raise SyntaxError(f"Erreur de syntaxe pour le calcul: \"{operation}\"")
@@ -12,5 +12,16 @@ while True:
         elif '-' in operation:
             int1, int2 = operation.split('-')
             result = int(int1.strip()) - int(int2.strip())
+        elif '*' in operation:
+            int1, int2 = operation.split('*')
+            result = int(int1.strip()) * int(int2.strip())
+        elif '/' in operation:
+            int1, int2 = operation.split('/')
+            result = int(int1.strip()) / int(int2.strip())
+        else :
+            raise SyntaxError(f"Erreur de syntaxe pour le calcul: \"{operation}\"")
+        
+        print(result)
 
-    
+    except Exception as e:
+        print(f"Erreur: {e}")
